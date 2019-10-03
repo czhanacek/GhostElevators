cee4daa15f7944f50394a079f297070828e6ead6
Charlie Hanacek <charlie@czhanacek.com>
2019-10-02 21:23:19 -0700

    updated the structure so it's better now

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
-FROM mhart/alpine-node:6 as buildstage
-RUN apk add --update git
-RUN git clone https://github.com/czhanacek/GhostElevators
-WORKDIR GhostElevators
-RUN npm install
-RUN npm run build
-
 FROM nginx as runstage
-COPY --from=buildstage GhostElevators/ /usr/share/nginx/html/
+COPY site/ /usr/share/nginx/html/
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
 
--- site/main.js
+++ site/main.js
@@ -266,7 +266,7 @@ function doMoveLoop(building, elevatorIndex) {
 
 function main() {
 	canvas = document.getElementById("mycanvas");
-	canvas.width = window.innerWidth;
+	canvas.width = window.innerHeight / 2; 
 	canvas.height = window.innerHeight;
 
 
@@ -276,7 +276,7 @@ function main() {
 	ctx.fillStyle = "#d8fff7";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 	var elevator1 = new Elevator(100,200, 200, 200, null);
-	var building = new Building(0, 0, canvas.width * 0.75, canvas.height * 0.75, 12, 20);
+	var building = new Building(0, 0, canvas.width * 0.75, canvas.height * 0.75, 30, 10);
 	setInterval(function() {
 		ctx.beginPath();
 		ctx.clearRect(0,0,canvas.width, canvas.height);
